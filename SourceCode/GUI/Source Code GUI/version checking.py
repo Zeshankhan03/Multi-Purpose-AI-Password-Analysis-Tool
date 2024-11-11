d8ac283c3ea94cb722465cd2a17138211810b811
Zeeshan Khan <zeshankhan03@users.noreply.github.com>
2024-11-11 07:28:23 +0500

    More modules installed

---
+++ SourceCode/GUI/Source Code GUI/version checking.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import sklearn
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+print("Pandas version:", pd.__version__)
+print("Scikit-learn version:", sklearn.__version__)
+print("Seaborn version:", sns.__version__)
+print("Matplotlib version:", plt.__version__)
